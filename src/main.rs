67ee138407789081647795cc97bc3be5930e5f8d
AnActualEmerald <emerald_actual@proton.me>
2022-08-11 12:31:54 -0400

    move to northstar-rcon-client

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,44 @@
-use std::io::{self, Write};
+// use std::io::{self, Write};
 
-use anyhow::Result;
-use crossterm::{cursor, execute, style::Print, terminal};
+// use anyhow::Result;
+use cursive::traits::*;
+use cursive::view::SizeConstraint;
+use cursive::views::Dialog;
+use cursive::Cursive;
 
-mod client;
-mod proto;
+// mod client;
+// mod proto;
 
 #[tokio::main]
 async fn main() {
-    let mut stdout = io::stdout();
-    if let Err(e) = run(&mut stdout).await {
-        println!("{}", e);
-    }
+    let mut siv = cursive::default();
+
+    siv.add_fullscreen_layer(
+        Dialog::text("This is a survey!\nPress <Next> when you're ready.")
+            .title("Important survey")
+            .button("Next", show_next)
+            .resized(SizeConstraint::Full, SizeConstraint::Full),
+    );
+
+    siv.run();
 }
 
-async fn run<W>(w: &mut W) -> Result<()>
-where
-    W: Write,
-{
-    execute!(
-        w,
-        terminal::EnterAlternateScreen,
-        terminal::Clear(terminal::ClearType::All),
-        cursor::MoveTo(0, 0)
-    )?;
-    execute!(
-        w,
-        Print(format!("Welcome to RetCon v{}!", env!("CARGO_PKG_VERSION"))),
-        cursor::MoveToNextLine(1)
-    )?;
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text("Did you do the thing?")
+            .title("Question 1")
+            .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
+            .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
+            .button("Uh?", |s| s.add_layer(Dialog::info("Try again!"))),
+    );
+}
 
-    //let the terminal exit the screen rather than return early on error
-    let ret = client::start().await;
-
-    execute!(w, terminal::LeaveAlternateScreen)?;
-
-    ret
+fn show_answer(s: &mut Cursive, msg: &str) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text(msg)
+            .title("Results")
+            .button("Finish", |s| s.quit()),
+    );
 }
--- src/proto.rs
+++
@@ -1,9 +0,0 @@
-//Include generated cl_rcon.proto
-pub mod cl_rcon {
-    include!(concat!(env!("OUT_DIR"), "/cl_rcon.rs"));
-}
-
-//Inlcude generated sv_rcon.proto
-pub mod sv_rcon {
-    include!(concat!(env!("OUT_DIR"), "/sv_rcon.rs"));
-}
